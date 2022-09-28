795ee8ee2cb777e545977ccfd29891fafa25cd6e
hosein <jamshidian1998@gmail.com>
2022-09-28 16:45:06 +0330

    remove comments from day1 and day2

--- Exercises/day1/e1.py
+++ Exercises/day1/e1.py
@@ -1,38 +1,38 @@
 # 1.0
 
-# print('Day 1 - Python Print Function \nThe Function is decleared like this :\nprint("what to print")')
+print('Day 1 - Python Print Function \nThe Function is decleared like this :\nprint("what to print")')
 
 # 1.1
 
-# print("ali"+"   "+"hosein"
+print("ali"+"   "+"hosein"
 
 # 1.2
 
-# print(Day 1 - String manioulation")
-# print("String Concatination is done with the "+" sign")
-#    print('e.g.print("hello"+"world")')
-# print(('new lines can be crated with a backslash and n')
+print(Day 1 - String manioulation")
+print("String Concatination is done with the "+" sign")
+   print('e.g.print("hello"+"world")')
+print(('new lines can be crated with a backslash and n')
 
 # 1.3
 
-# print("the length of youre name is :",len(input("what is youre name?").strip()))
+print("the length of youre name is :",len(input("what is youre name?").strip()))
 
 # 1.4
 
-# a=int(input("a:"))
-# b=int(input("b:"))
-# a,b=b,a
-# print("a :",a,"\nb:",b)
-#       OR
-# a=int(input("a:"))
-# b=int(input("b:"))
-# temp=a
-# a=b
-# b=temp
-# print("a :",a,"\nb:",b)
+a=int(input("a:"))
+b=int(input("b:"))
+a,b=b,a
+print("a :",a,"\nb:",b)
+      OR
+a=int(input("a:"))
+b=int(input("b:"))
+temp=a
+a=b
+b=temp
+print("a :",a,"\nb:",b)
 
 # Final
-#
+
 print("Welcome To band Name Generator!")
 city=input("please inser you're city that grew up there:\n").lower()
 pet=input("just one more thing :).what is the name of you're favorite pet:\n").lower()
